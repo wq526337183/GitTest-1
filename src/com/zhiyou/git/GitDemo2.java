2d9ae908bc96631953fb713c4c307eccc732ad81
liusiyuan <708959090@qq.com>
2018-01-05 16:48:14 +0800

    git-test-two

---
+++ src/com/zhiyou/git/GitDemo2.java
@@ -0,0 +1,15 @@
+package com.zhiyou.git;
+
+/**  
+* @ClassName: GitDemo2  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:46:49
+*  
+*/
+public class GitDemo2 {
+
+	public static void main(String[] args) {
+		System.out.println("git02");
+	}
+}
